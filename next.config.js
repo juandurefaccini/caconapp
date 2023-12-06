72d43e8d0b3e08b59122af277dd5675201d2489e
Juan Dure Faccini <juanchidure@outlook.com>
2023-12-06 01:26:30 -0300

    PWA SUPPORT

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// Note : To required('next-pwa') you should install that package as explain
+// above. If you miss that please install it with npm i next-pwa command.
 
-module.exports = nextConfig
+const withPWA = require("next-pwa")({
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+});
+
+const nextConfig = withPWA({
+    reactStrictMode: true,
+    swcMinify: true,
+});
+
+module.exports = nextConfig;
